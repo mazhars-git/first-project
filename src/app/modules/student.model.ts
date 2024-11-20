448997630464ef91a368e63151226e68586b1923
mazhars-git <mazharulislam611@gmail.com>
2024-11-21 00:13:57 +0600

    up

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -1,21 +1,32 @@
-import { Schema, model, connect } from 'mongoose';
-import { Student } from './student/student.interface';
+import { Schema } from 'mongoose';
+import { Guardian, Student, UserName } from './student/student.interface';
+
+const userNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  midName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  fatherContact: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherOccupation: { type: String, required: true },
+  motherContact: { type: String, required: true },
+});
 
 const studentSchema = new Schema<Student>({
   id: { type: String },
-  name: {
-    firstName: {
-      type: String,
-      required: true,
-    },
-    midName: {
-      type: String,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-  },
+  name: userNameSchema,
   gender: ['male', 'female'],
   dateOfBirth: { type: String },
   email: {
@@ -30,7 +41,7 @@ const studentSchema = new Schema<Student>({
   bloodGroup: ['A+', 'A-', 'B+', 'B-'],
   presentAddress: { type: String, required: true },
   permanentAddress: { type: String, required: true },
-  guardian: {},
+  guardian: guardianSchema,
   localGuardian: {},
   profileImg: { type: String },
   isActive: ['active', 'inActive'],
