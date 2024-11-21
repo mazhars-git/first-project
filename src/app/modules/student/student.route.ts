491db2a8e8e24f295f445f46b6e464ecb8d3b50f
mazhars-git <mazharulislam611@gmail.com>
2024-11-21 22:06:43 +0600

    added data

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -2,5 +2,6 @@ import express from 'express';
 import { StudentControllers } from './student.controller';
 const router = express.Router();
 
-// call controller function
 router.post('/create-student', StudentControllers.createStudent);
+
+export const StudentRoutes = router;
