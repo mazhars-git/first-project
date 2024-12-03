31c382dcb38c00e0fee3da6ae6ae2d3342baa349
mazhars-git <mazharulislam611@gmail.com>
2024-12-03 19:10:49 +0600

    route

---
+++ src/app/routes/index.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { UserRoutes } from '../modules/user/user.route';
+import { StudentRoutes } from '../modules/student/student.route';
+
+const router = Router();
+
+const moduleRouters = [
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+  {
+    path: '/students',
+    route: StudentRoutes,
+  },
+];
+
+moduleRouters.forEach((route) => router.use(route.path, route.route));
+
+export default router;
