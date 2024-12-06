bb468e8c524dc9e7e3d84da887546d1d038bbd8c
mazhars-git <mazharulislam611@gmail.com>
2024-12-06 19:43:12 +0600

    up

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,9 +1,17 @@
 import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
+import { User } from './user.model';
 
-const findLastStudentId = async () => {};
+const findLastStudentId = async () => {
+  const lastStudent = await User.findOne(
+    {
+      role: 'student',
+    },
+    {},
+  );
+};
 
 export const generateStudentId = (payLoad: TAcademicSemester) => {
-  const currentId = (0).toString().padStart(4, '0');
+  const currentId = (0).toString();
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payLoad.year}${payLoad.code}${incrementId}`;
 
