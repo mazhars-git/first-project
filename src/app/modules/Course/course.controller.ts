af67ce22475b0457f90fb1d9c8bb6ece1a3e1d9f
mazhars-git <mazharulislam611@gmail.com>
2024-12-14 00:16:06 +0600

    up

---
+++ src/app/modules/Course/course.controller.ts
@@ -0,0 +1,53 @@
+// import { UserServices } from './user.service';
+import sendResponse from '../../utils/sendRes';
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import { AcademicFacultyServices } from './academicFaculty.service';
+import { CourseServices } from './course.service';
+
+const createCourse = catchAsync(async (req, res) => {
+  const result = await CourseServices.createCourseIntoDB(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Course is created successfully',
+    data: result,
+  });
+});
+
+const getAllCourses = catchAsync(async (req, res) => {
+  const result = await CourseServices.getAllCourseFromDB();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Courses are retrieved successfully',
+    data: result,
+  });
+});
+const getSingleCourse = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await CourseServices.getSingleCourseFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Course retrieved successfully',
+    data: result,
+  });
+});
+const deleteCourse = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await CourseServices.deleteCourseFromDB(id, req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Course deleted successfully',
+    data: result,
+  });
+});
+
+export const CourseController = {
+  createCourse,
+  getAllCourses,
+  getSingleCourse,
+  deleteCourse,
+};
