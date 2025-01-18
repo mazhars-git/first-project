5f71ef905aa1191cdf0ec90b71d3d77e118e808e
mazhars-git <mazharulislam611@gmail.com>
2025-01-18 23:22:20 +0600

    update

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -15,10 +15,15 @@ const auth = (...requiredRoles: TUserRole[]) => {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
     }
     // check if the token is valid
-    const decoded = jwt.verify(
-      token,
-      config.jwt_access_secret as string,
-    ) as JwtPayload;
+    let decoded;
+    try {
+      decoded = jwt.verify(
+        token,
+        config.jwt_access_secret as string,
+      ) as JwtPayload;
+    } catch (err) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'Unauthorized');
+    }
 
     const { role, userId, iat } = decoded;
 
